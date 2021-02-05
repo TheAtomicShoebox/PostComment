1edecd8706a8052320b9d24a065b44728e8f6e93
TheAtomicShoebox <33351047+TheAtomicShoebox@users.noreply.github.com>
2021-02-04 21:06:26 -0500

    Merge

--- PostComment.Data/Reply.cs
+++ PostComment.Data/Reply.cs
@@ -7,21 +7,8 @@ using System.Threading.Tasks;
 
 namespace PostComment.Data
 {
-    class Reply : Comment
-    {
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9b065a6f20d49919f6288b44925f01a23ec3e57a
-
-    public Reply(int id, string text, Guid _userID)
+    public class Reply : Comment
     {
 
     }
-<<<<<<< HEAD
-=======
->>>>>>> Reply class initial commit
-=======
->>>>>>> 9b065a6f20d49919f6288b44925f01a23ec3e57a
 }
