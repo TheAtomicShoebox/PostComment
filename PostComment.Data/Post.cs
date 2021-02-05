96ef4c640b1e9cf2ec3724953c00761a696e6f11
Amy Martinez <ajmartinez0712@gmail.com>
2021-02-04 20:05:13 -0500

    Post class

---
+++ PostComment.Data/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostComment.Data
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        
+        public virtual List<Comment> Comment { get; set; }
+
+        [Required]
+        public Guid Author { get; set; }
+
+        public Post(int id, string text, Guid _userId)
+        {
+            Comment = new List<Comment>();
+
+        }
+    }
+}
