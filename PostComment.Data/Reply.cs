ddc455ddac8b5c5ef041ddbaf05937820d92976c
TheAtomicShoebox <33351047+TheAtomicShoebox@users.noreply.github.com>
2021-02-06 12:15:45 -0500

    ReplyCreate, Service, Controller, Swagger Install

--- PostComment.Data/Reply.cs
+++ PostComment.Data/Reply.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PostComment.Data
 {
     public class Reply : Comment
     {
+        [Required]
+        [ForeignKey(nameof(Comment))]
+        public int ParentCommentId { get; set; }
+
+        public virtual Comment Comment { get; set; }
+        
         public Reply() : base()
         {
 
---
+++ PostComment.Models/ReplyService.cs
@@ -0,0 +1,38 @@
+﻿using PostComment.Data;
+using PostComment.WebAPI.Models;
+using System;
+
+namespace PostComment.Models
+{
+    public class ReplyService
+    {
+        private readonly Guid _userId;
+
+        public ReplyService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateReply(Post post, Comment comment, ReplyCreate model)
+        {
+            var entity =
+                new Reply()
+                {
+                    Author = _userId,
+                    Comment = comment,
+                    ParentCommentId = comment.Id,
+                    Post = post,
+                    PostId = post.Id,
+                    Text = model.Text
+                };
+
+            using(var ctx = new ApplicationDbContext())
+            {
+                comment.ReplyChain.Add(entity);
+                ctx.Comments.Add(entity);
+
+                return ctx.SaveChanges() == 2;
+            }
+        }
+    }
+}
